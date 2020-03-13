f76e5b2b97dc33061f04d52d1b24da43e0f3da39
IvanSabV <60038980+IvanSabV@users.noreply.github.com>
2020-03-14 02:36:47 +0400

    main + tab

--- source/js/script.js
+++ source/js/script.js
@@ -1,45 +1,15 @@
-var orderButton = document.querySelector('.features__button');
-var orderIcon = document.querySelectorAll('.item__icon');
-
-var cartPopup = document.querySelector('.cart-add');
-var cartDialog = cartPopup.querySelector('.cart-add__wrapper');
-
-var catalogList = document.querySelector('.catalog__list');
-
-if(orderButton) {
-  orderButton.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    cartPopup.classList.remove('cart-add--closed');
-  });
-} else {
-  for (var i = 0; i < orderIcon.length; i++) {
-    orderIcon[i].onclick = function() {
-      cartPopup.classList.remove('cart-add--closed');
-    };
+function tabs(evt, typeTab) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(typeTab).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
-cartPopup.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.add('cart-add--closed');
-});
-
-cartDialog.addEventListener('click', function (evt) {
-  evt.stopImmediatePropagation();
-});
-
-
-var navMain = document.querySelector('.nav-main');
-var navIcon = document.querySelector('.nav-main__icon');
-
-navMain.classList.remove('nav-main--nojs');
-
-navIcon.addEventListener('click', function() {
-  if (navMain.classList.contains('nav-main--closed')) {
-    navMain.classList.remove('nav-main--closed');
-    navMain.classList.add('nav-main--opened');
-  } else {
-    navMain.classList.add('nav-main--closed');
-    navMain.classList.remove('nav-main--opened');
-  }
-});
+document.getElementById("defaultOpen").click();
